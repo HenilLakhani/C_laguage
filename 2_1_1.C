dbb02100b0ee8e7798fbe6bf87b2dd8176cb2c25
Henil lakhani <lakhanihenil113@gmail.com>
2022-09-21 09:21:04 +0530

    project upload

---
+++ 2_1_1.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c,d,e,f,g;
+	clrscr();
+	a=10;
+	b=25;
+	c=a+b;
+	d=a*b;
+	e=a/b;
+	f=a-b;
+	g=a%b;
+	printf("sum :-%d\n",c);
+	printf("div :-%d\n",d);
+	printf("multi :-%d\n",e);
+	printf("sub :-%d\n",f);
+	printf("modulo :-%d",g);
+	getch();
+}
